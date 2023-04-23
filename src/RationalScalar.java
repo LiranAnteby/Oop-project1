6bac17637a2b43b38047b099066df12894f7a4de
Liran Anteby <liranan@post.bgu.ac.il>
2023-04-23 11:39:52 +0300

    great morning! finish Rational and Integer.

--- src/RationalScalar.java
+++ src/RationalScalar.java
@@ -1,4 +1,3 @@
-import java.util.NoSuchElementException;
 
 public class RationalScalar implements Scalar {
     private final int numerator;
@@ -8,47 +7,75 @@ public class RationalScalar implements Scalar {
         this.numerator = numerator;
         this.denominator = denominator;
     }
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
     public Scalar add(Scalar s) {
-        throw new NoSuchElementException();
+        return s.add(this);
+    }
+    private Scalar add(IntegerScalar s){
+        return this.add(new RationalScalar(s.getNumber(),1));
+    }
+    private Scalar add(RationalScalar s){
+        int tempN = numerator * s.getDenominator() + s.getDenominator() * denominator;
+        int tempD = denominator * s.getDenominator();
+        if (tempN%tempD == 0) return new IntegerScalar(tempN/tempD);
+        return new RationalScalar(tempN, tempD).reduce();
     }
     public Scalar mul(Scalar s){
-        throw new NoSuchElementException();
+        return s.mul(this);
+    }
+    private Scalar mul(IntegerScalar s){
+        return this.mul(new RationalScalar(s.getNumber(),1));
+    }
+    private Scalar mul(RationalScalar s){
+        int tempN = numerator * s.getNumerator();
+        int tempD = denominator * s.getDenominator();
+        if (tempN%tempD == 0) return new IntegerScalar(tempN/tempD);
+        return new RationalScalar(tempN,tempD).reduce();
     }
 
     public Scalar neg(){
-        return new RationalScalar(-numerator,denominator);
+        return new RationalScalar(-numerator,denominator).reduce();
     }
 
     public Scalar power(int exponent){
         int tempN = (int)Math.pow(numerator,exponent);
         int tempD = (int)Math.pow(denominator,exponent);
-        return new RationalScalar(tempN,tempD);
+        return new RationalScalar(tempN,tempD).reduce();
     }
 
     public int sign(){
         if (numerator == 0) return 0;
-        if ((numerator > 0 && denominator > 0) | (numerator < 0 && denominator < 0)) return 1;
+        if ((numerator > 0 & denominator > 0) | (numerator < 0 & denominator < 0)) return 1;
         return -1;
     }
 
     public boolean equals(Object o){
         if (! (o instanceof Scalar)) return false;
-        return true;
+        if (o instanceof IntegerScalar)
+            return (numerator / denominator == ((IntegerScalar)o).getNumber()) ;
+        else
+            return ( ((RationalScalar)o).getNumerator()/((RationalScalar)o).getDenominator() == numerator/denominator );
     }
     public String toString(){
-        RationalScalar reducedThis = new RationalScalar(numerator,denominator).reduce();
-        int reducedN = reduce().numerator , reducedD = reducedThis.denominator;
-        if (reducedD == 1 | reducedD == -1)
-            return "" + reducedN;
+        RationalScalar reducedThis = this.reduce();
+        int reducedN = reducedThis.numerator , reducedD = reducedThis.denominator;
+        if (reducedD == 1) return "" + reducedN;
+        if (reducedD == -1) return "" + -reducedN;
         if(reducedThis.sign() == -1)
             return "-" + Math.abs(reducedN) + "/" + Math.abs(reducedD);
-        return "" + Math.abs(reducedN) + "/" + Math.abs(reducedD);
+        return "" + reducedN + "/" + reducedD;
 
     }
-    // using the GCD Algorithm.
+    // using a GCD Algorithm.
     public RationalScalar reduce(){
         if (numerator == 0) return new RationalScalar(0,1);
-        int gcd = 1, tempN = numerator, tempD = denominator;
+        int tempN = numerator, tempD = denominator;
         boolean isNegN = false, isNegD = false;
         if (tempN<0) {
             tempN = -tempN;
@@ -67,7 +94,7 @@ public class RationalScalar implements Scalar {
                 tempD = tempD - tempN;
         }
         if (isNegD & isNegN)
-            gcd = -gcd;
-        return new RationalScalar(numerator/gcd,denominator/gcd);
+            tempD = -tempD;
+        return new RationalScalar(numerator/tempD,denominator/tempD);
     }
 }
