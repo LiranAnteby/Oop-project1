dd83c720d5b1277902dbd8f80ef4808ba9cd08ce
Shai Hubashi <shaihu@post.bgu.ac.il>
2023-04-28 17:45:55 +0300

    fix PolynomialTest

--- Tests/PolynomialTest.java
+++ Tests/PolynomialTest.java
@@ -7,6 +7,9 @@ class PolynomialTest {
     private Polynomial polynomial4;
     private Polynomial polynomial5;
     private Polynomial polynomial6;
+    private Polynomial p7;
+    private Polynomial p8;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         polynomial1 = Polynomial.build("1 2 3");
@@ -15,10 +18,14 @@ class PolynomialTest {
         polynomial4 = Polynomial.build("5");
         polynomial5 = Polynomial.build("1 -2 3");
         polynomial6 = Polynomial.build("0 1/2 3 -5/3");
+        p7 = Polynomial.build("1 3 5 3");
+        p8 = Polynomial.build("2 6");
+
     }
 
     @org.junit.jupiter.api.Test
     void build() {
+        assertEquals(polynomial1,polynomial1);
         assertEquals("1+2x+3x^2",polynomial1.toString());
         assertEquals("1x+2x^2+3x^3",polynomial2.toString());
         assertEquals("7x^6",polynomial3.toString());
@@ -29,6 +36,7 @@ class PolynomialTest {
 
     @org.junit.jupiter.api.Test
     void test_add() {
+        assertEquals(p7,polynomial1.add(polynomial2));
         assertEquals("1+3x+5x^2+3x^3",polynomial1.add(polynomial2).toString());
         assertEquals("5+1/2x+3x^2-5/3x^3",polynomial4.add(polynomial6).toString());
         assertEquals("1-2x+3x^2+7x^6",polynomial5.add(polynomial3).toString());
@@ -42,6 +50,7 @@ class PolynomialTest {
 
     @org.junit.jupiter.api.Test
     void test_evaluate() {
+        assertEquals(new IntegerScalar(86),polynomial1.evaluate(new IntegerScalar(5)));
         assertEquals("86",polynomial1.evaluate(new IntegerScalar(5)).toString());
         assertEquals("448",polynomial3.evaluate(new IntegerScalar(2)).toString());
         assertEquals("7",polynomial3.evaluate(new IntegerScalar(1)).toString());
@@ -49,6 +58,8 @@ class PolynomialTest {
         assertEquals("-1/3",polynomial6.evaluate(new IntegerScalar(2)).toString());
         assertEquals("-249",polynomial6.evaluate(new IntegerScalar(6)).toString());
         assertEquals("7/729",polynomial3.evaluate(new RationalScalar(1, 3)).toString());
+        assertEquals(new RationalScalar(7,729),polynomial3.evaluate(new RationalScalar(1, 3)));
+
     }
 
     @org.junit.jupiter.api.Test
